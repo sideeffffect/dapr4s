c606bdfe85859ebe47979dcc7c56b3b03f7f2656
Ondra Pelech <ondra.pelech@gmail.com>
2026-05-02 11:40:05 +0200

    -Wconf:any:error

--- project.scala
+++ project.scala
@@ -4,6 +4,7 @@
 //> using options "-Ycc-verbose"
 //> using options "-Yexplicit-nulls"
 //> using options "-experimental"
+//> using options "-Wconf:any:error"
 // Note: -language:experimental.safe is NOT applied globally because non-safe-mode
 // files (DaprRuntime, JsonCodec, internal/*) need to use @scala.caps.assumeSafe.
 // Safe mode is enabled per-file via: import language.experimental.safe
